38c20397b36122f0911836dd2b9f2ec38b7bbc4b
user-vi <50022091+user-vi@users.noreply.github.com>
2020-01-08 19:18:33 +0300

    upd

--- JenkinsTest/testR.R
+++ JenkinsTest/testR.R
@@ -1,4 +1,4 @@
 # print('hello, I am R script')
-# write("prints to stdout", stdout())
-cars <- c(1,3,6,10)
-write.csv(cars, 'cars.csv')
+write("seva", stdout())
+# cars <- c(1,3,6,10)
+# write.csv(cars, 'cars.csv')
